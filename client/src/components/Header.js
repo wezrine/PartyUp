730a9ee85a3d216829c62b64740febb820afcdbb
William Ezrine <williamezrine@williams-mbp.lan>
2021-05-11 10:40:30 -0400

    landing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,25 @@
 import logo from './logo.png'
-import logoWhite from './logoWhite.png'
+
 import { NavLink } from 'react-router-dom'
 
 function Header() {
 
     return (
-            <nav className="navbar level is-dark">
+            <nav className="navbar level is-black">
                 <p className="level-item has-text-centered">
-                    <NavLink to='/search' className="link is-info">Search</NavLink>
+                    <NavLink to='/search' className="link">Steam</NavLink>
                 </p>
                 <p className="level-item has-text-centered">
-                    <NavLink to='/games' className="link is-info">Games</NavLink>
+                    <NavLink to='/games' className="link">Games</NavLink>
                 </p>
                 <p className="level-item has-text-centered">
-                    <img className="logo" src={logoWhite} alt="" />
+                    <img className="logo" src={logo} alt="" />
                 </p>
                 <p className="level-item has-text-centered">
-                    <NavLink to='/groups' className="link is-info">Groups</NavLink>
+                    <NavLink to='/groups' className="link">Party</NavLink>
                 </p>
                 <p className="level-item has-text-centered">
-                    <NavLink to='/profile' className="link is-info">Profile</NavLink>
+                    <NavLink to='/profile' className="link">Profile</NavLink>
                 </p>
             </nav>
 
