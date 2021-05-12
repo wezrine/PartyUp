f1b70d57d3cb2c07e958e236bad4f77ed44cef17
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-12 16:02:14 -0400

    Made api non working api call on the server. Made Search.js and Search DropdownItem.js in client

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 // API KEY: AA3FAF927A5D3C878127978E156DBE73
 
 const express = require('express')
-const app = express() 
+const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+// const fetch = require('node-fetch')
+const axios = require('axios')
+const rawgApiKey = process.env.RAWG_API_KEY
 
 // Schema
 const Party = require('./schemas/party')
@@ -15,7 +18,7 @@ app.use(express.json())
 mongoose.connect('mongodb+srv://wezrine:alexander@cluster0.nxus8.mongodb.net/PartyUp?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true
 }, (error) => {
-    if(!error) {
+    if (!error) {
         console.log('Successfully connected to MongoDB Database')
     } else {
         console.log(error)
@@ -25,9 +28,9 @@ mongoose.connect('mongodb+srv://wezrine:alexander@cluster0.nxus8.mongodb.net/Par
 app.get('/party/:appId', (req, res) => {
     let appId = req.params.appId
 
-    Party.find({'appId': appId}, (error, parties) => {
-        if(error) {
-            res.json({error: 'Unable to get parties'})
+    Party.find({ 'appId': appId }, (error, parties) => {
+        if (error) {
+            res.json({ error: 'Unable to get parties' })
         } else {
             res.json(parties)
         }
@@ -48,13 +51,47 @@ app.post('/party', (req, res) => {
     })
 
     party.save((error) => {
-        if(error) {
-            res.json({error: 'Unable to save!'})
+        if (error) {
+            res.json({ error: 'Unable to save!' })
         } else {
-            res.json({success: true, message: 'Saved new post'})
+            res.json({ success: true, message: 'Saved new post' })
         }
     })
 })
+
+
+// Autocomplete api call
+const options = {
+    method: 'GET',
+    url: 'https://api.rawg.io/api/games?tags=multiplayer',
+    qs: {
+        key: rawgApiKey,
+        search: undefined
+    }
+};
+
+const apiCall = async (url, options) => {
+    await axios(url, options)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`The HTTP status of the reponse: ${res.status} (${res.statusText})`);
+            }
+            return res.json();
+        })
+        .then(json => console.log(json.results[1]))
+        .catch(err => console.log(err))
+}
+
+apiCall(options)
+
+app.get('/api/gameAutocomplete', async (req, res) => {
+    const query = req.query.q;
+    gameAutocomplete.qs.search = query;
+    res.set('Cache-Control', 'no-cache');
+    res.json(await apiCall(gameAutocomplete));
+    console.log(`/api/gameAutocomplete?q=${query} endpoint has been called!`);
+});
+
 
 app.listen(8080, () => {
     console.log('Server is running...')
