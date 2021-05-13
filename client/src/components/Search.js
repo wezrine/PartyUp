a0d1d19bdc6e1e17d7b845ad41ced1164c05d15c
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-13 13:44:24 -0400

    backendApiCall

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -10,7 +10,7 @@ function Search(props) {
     async function getRawgApi(gameName) {
         if (gameName !== '') {
             try {
-                const response = await fetch(`https://api.rawg.io/api/games?key=00b7a7b22bfb4713982e46a6c4c79bf3&tags=multiplayer&search=${gameName.toLowerCase()}`);
+                const response = await fetch(`http://localhost:8080/api/search?search=${gameName.toLowerCase()}`);
                 const json = await response.json();
                 setData(json);
                 setDataIsReady(true);
--- server/app.js
+++ server/app.js
@@ -10,13 +10,14 @@ const axios = require('axios')
 const rawgApiKey = process.env.RAWG_API_KEY
 const userRouter = require('./routes/user')
 
+
 // Schema
 const Party = require('./schemas/party')
 
 app.use(cors())
 app.use(express.json())
 app.use('/user', userRouter)
-
+app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect('mongodb+srv://wezrine:alexander@cluster0.nxus8.mongodb.net/PartyUp?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -63,7 +64,7 @@ app.post('/party', (req, res) => {
 })
 
 
-// // Autocomplete api call
+// Autocomplete api call
 // const options = {
 //     method: 'GET',
 //     url: 'https://api.rawg.io/api/games?tags=multiplayer',
@@ -85,7 +86,16 @@ app.post('/party', (req, res) => {
 //         .catch(err => console.log(err))
 // }
 
-// apiCall(options)
+// async function getRawgApi(gameName) {
+//     if (gameName !== '') {
+//         try {
+//             const response = await fetch(`https://api.rawg.io/api/games?key=00b7a7b22bfb4713982e46a6c4c79bf3&tags=multiplayer&search=${gameName.toLowerCase()}`);
+//             const json = await response.json();
+//         } catch (err) {
+//             console.error(err);
+//         }
+//     }
+// }
 
 // app.get('/api/gameAutocomplete', async (req, res) => {
 //     const query = req.query.q;
@@ -95,6 +105,45 @@ app.post('/party', (req, res) => {
 //     console.log(`/api/gameAutocomplete?q=${query} endpoint has been called!`);
 // });
 
+app.get('/api/search', async(req, res) => {
+    // const gameName = req.body.gameName
+    // console.log(gameName)
+    const searchURL = `https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&tags=multiplayer&search=${req.query.search}`
+    try {
+        const {data} = await axios.get(searchURL)
+        res.json(data)
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    // fetch(`https://api.rawg.io/api/games?key=00b7a7b22bfb4713982e46a6c4c79bf3&tags=multiplayer&search=${gameName.toLowerCase()}`)
+    // .then(response => response.json())
+    // .then(json => {
+    //     console.log(json.results)
+    // })
+// const apiCall = async (url) => {
+//     try {
+//         const response = await axios.get(url)
+//         console.log(response)
+//         const json = await response.json()
+//         res.json(json.results)
+//     } catch (error) {
+//         console.log(error)
+//     }
+//     // const response = await axios(url)
+//     //     .then(res => {
+//     //         if (!res.ok) {
+//     //             throw new Error(`The HTTP status of the reponse: ${res.status} (${res.statusText})`);
+//     //         }
+//     //         return res.json();
+//     //     })
+//     //     .then(json => console.log(json.results[1]))
+//     //     .catch(err => console.log(err))
+// }
+// apiCall(searchURL)
+})
 
 app.listen(8080, () => {
     console.log('Server is running...')
