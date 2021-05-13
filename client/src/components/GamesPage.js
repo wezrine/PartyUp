3fd78e8c9026523df5081efe722803367f6ed17c
Shawn Saj <76454973+snmsaj@users.noreply.github.com>
2021-05-13 10:28:11 -0400

    applied filters to the background image

--- client/src/components/GamesPage.js
+++ client/src/components/GamesPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import GamesList from './GamesList'
 import Search from './Search';
 
@@ -10,25 +10,30 @@ function GamesPage() {
     const getParties = (game) => {
 
         setBackgroundURL(game.background_image)
-    
+
         fetch(`http://localhost:8080/party/${game.name}`)
-        .then(response => response.json())
-        .then(parties => {
-            setParties(parties)
-        })
+            .then(response => response.json())
+            .then(parties => {
+                setParties(parties)
+            })
     }
 
     // console.log(gameBackground)
 
     return (
-        <div className="page-container" style={{backgroundImage: `url(${backgroundURL})`}}>
-            <div className="is-flex is-justify-content-center searchbar-container">
-                <div className="is-flex searchbar">
-                    <Search gameClicked={getParties} />
+        <Fragment>
+            <div className="page-container" style={{ backgroundImage: `linear-gradient(to bottom, rgba(27,40,56) 0%,rgba(27,40,56,0.7) 100%), url(${backgroundURL})`, }}>
+            
+                <div className="find-party-content">
+                    <div className="is-flex is-justify-content-center searchbar-container">
+                        <div className="is-flex searchbar">
+                            <Search gameClicked={getParties} />
+                        </div>
+                    </div>
+                    <GamesList parties={parties} />
                 </div>
             </div>
-            <GamesList parties = {parties}/>
-        </div>
+        </Fragment>
     )
 }
 
