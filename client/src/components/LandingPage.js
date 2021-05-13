bfb9e5ce3b4bd813bd6a099125ca8a0dad25cddb
William Ezrine <williamezrine@williams-mbp.lan>
2021-05-13 11:15:16 -0400

    navbar

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {
                 <div className="hero-body">
                     <div className="container has-text-centered">
                         <h1 className="title">Hello, Gamers!</h1>
-                        <h2 className="subtitle">Find friends, because we know you dont have any!</h2>
+                        <h2 className="subtitle">Form a party, because we know you've never been to one!</h2>
                         <button className="button is-info">Get Started</button>
                     </div>
                 </div>
