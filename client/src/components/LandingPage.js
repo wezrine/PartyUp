43c689d974b6acf594777e0826bfe84e585e0137
Clarence Saxby <csaxby318@gmail.com>
2021-05-14 16:29:55 -0400

    removed Get Started button from LandingPage for authenticated users. also changed where users are routed to upon login

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,17 +1,24 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function LandingPage() {
+function LandingPage(props) {
     return (
             <section className="hero is-dark is-fullheight">
                 <div className="hero-body">
                     <div className="container has-text-centered">
                         <h1 className="title">Hello, Gamers!</h1>
                         <h2 className="subtitle">Form a party, because we know you've never been to one!</h2>
-                        <NavLink to='/login' className="button is-info">Get Started</NavLink>
+                        {props.isAuthenticated ? null : <NavLink to='/login' className="button is-info">Get Started</NavLink>}
                     </div>
                 </div>
             </section>
     )
 }
 
-export default LandingPage
+const mapStateToProps = (state) => {
+    return {
+                    isAuthenticated: state.isAuthenticated
+    }
+}
+
+export default connect(mapStateToProps)(LandingPage)
